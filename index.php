a9ee24cda6f3db8e8f90f649c568356e00ddc6b1
KutovyiAO <Kutovyiao@Gmail.com>
2017-03-03 15:46:44 +0200

    HTML first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+    <meta charset="utf-8">
+    <title>Registration form</title>
+</head>
+
+
+<body>
+
+<p>Введите регистрационные данные</p><br>
+
+<p>ID</p>
+<input name="text" >
+<br>
+<p>name</p>
+<input name="text" >
+<br>
+<p>description</p>
+<input name="text" >
+<br>
+<p>created_at</p>
+<input name="text" >
+
+</body>
+
+
+</html>
+
+
